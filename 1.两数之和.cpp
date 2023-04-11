74d29abe08ef076976d300904851603be6536e74
ma-wanyi <13422422919@163.com>
2023-04-12 01:04:15 +0800

    23-4-12

--- 1.两数之和.cpp
+++ 1.两数之和.cpp
@@ -5,10 +5,24 @@
  */
 
 // @lc code=start
+#include <vector>
+using namespace std;
+
+// 暴力解法
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        
+        vector<int> result;
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i+1; j < nums.size(); j++) {
+                if (nums[i]+nums[j] == target) {
+                    result.push_back(i);
+                    result.push_back(j);
+                    break;
+                }
+            }
+        }
+        return result;
     }
 };
 // @lc code=end
